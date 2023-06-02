dc7405c388b36b6f3df232c4bf6dea593e7762fa
Greenlight5 <95604827+Greenlight5@users.noreply.github.com>
2023-06-02 16:20:01 +0300

    Update airbnb quiz.py

--- airbnb quiz.py
+++ airbnb quiz.py
@@ -1,3 +1,14 @@
-print('hello you!')
-x = input("choose a number")
-print('OK, Bye!')
+print('welcome! please fill the quizz and see the results')
+
+a = input("Enter the city:")
+b = input("How many people:")
+c = input("Your trip is in the weekday or weekend?")
+d = input("would you like to stay close to the center  , middle or far?")
+#e = input("")
+#f = input("")
+#g = input("")
+#h = input("")
+#i = input("")
+#j = input("")
+
+print('We try our best to give you the best room , Wait for your results')
